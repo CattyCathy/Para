6f74de5fddefb5e2a8e8d778dbfe527d7f82bbc9
CattyCathy <157694740+CattyCathy@users.noreply.github.com>
2025-05-27 00:45:33 +0800

    Update TextBlock

--- Para/UI/Control/TextBox.cs
+++ Para/UI/Control/TextBox.cs
@@ -5,6 +5,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+using System.Windows.Media;
 
 namespace Para.UI.Control
 {
@@ -12,12 +16,21 @@ namespace Para.UI.Control
     {
         public Caret _caret = new();
         protected string _text = string.Empty;
-        protected List<SpriteText> _spriteTexts = [];
+        protected List<SpriteChar> _spriteTexts = [];
+
+        private StackPanel? _charPanel;
 
         public string Text
         {
             get => _text;
-            set => _text = value;   
+            set
+            {
+                if (_text != value)
+                {
+                    _text = value;
+                    UpdateSpriteChars();
+                }
+            }
         }
 
         static TextBox()
@@ -30,6 +43,30 @@ namespace Para.UI.Control
         public TextBox()
         {
             Loaded += TextBox_Loaded;
+            Focusable = true;
+            this.PreviewKeyDown += TextBox_PreviewKeyDown;
+            this.PreviewTextInput += TextBox_PreviewTextInput;
+        }
+
+        private void TextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Back && Text.Length > 0)
+            {
+                Text = Text.Substring(0, Text.Length - 1);
+                e.Handled = true;
+            }
+        }
+
+        protected override void OnMouseDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseDown(e);
+            this.Focus();
+        }
+
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Text += e.Text;
+            e.Handled = true;
         }
 
         private void TextBox_Loaded(object sender, RoutedEventArgs e)
@@ -44,5 +81,58 @@ namespace Para.UI.Control
             _caret.Width = DesignDetail.Control.TextBox.CaretWidth;
             _caret.Height = (double)this.FontSize;
         }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            _charPanel = GetTemplateChild("PART_CharPanel") as StackPanel;
+            UpdateSpriteChars();
+        }
+
+        private void UpdateSpriteChars()
+        {
+            if (_charPanel == null) return;
+            var oldSprites = _spriteTexts.ToList();
+            _charPanel.Children.Clear();
+            _spriteTexts.Clear();
+
+            int i = 0;
+            for (; i < _text.Length; i++)
+            {
+                SpriteChar spriteChar;
+                if (i < oldSprites.Count && oldSprites[i].Char == _text[i])
+                {
+                    spriteChar = oldSprites[i];
+                }
+                else
+                {
+                    spriteChar = new SpriteChar
+                    {
+                        Char = _text[i],
+                        FontSize = this.FontSize,
+                        Foreground = this.Foreground,
+                        HasShadow = DesignDetail.Text.SpriteText.HasShadow,
+                        ShadowColor = DesignDetail.Text.SpriteText.ShadowColor,
+                        ShadowOffset = DesignDetail.Text.SpriteText.ShadowOffset
+                    };
+                }
+                _spriteTexts.Add(spriteChar);
+                _charPanel.Children.Add(spriteChar);
+            }
+
+            for (; i < oldSprites.Count; i++)
+            {
+                var toDelete = oldSprites[i];
+                _charPanel.Children.Add(toDelete);
+                toDelete.PlayDeleteAnimation(() =>
+                {
+                    _charPanel.Children.Remove(toDelete);
+                });
+            }
+
+            _charPanel.Children.Add(_caret);
+        }
+
+
     }
 }
---
+++ Para/UI/Text/SpriteChar.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace Para.UI.Text
+{
+    public class SpriteChar : BeatSyncedControl
+    {
+        public bool HasShadow;
+        public System.Windows.Media.Color ShadowColor;
+        public Vector2 ShadowOffset;
+        private char _char = ' ';
+
+        private double _interval;
+        private DateTime _animationStartTime = DateTime.Now;
+
+        public char Char
+        {
+            get => _char;
+            set
+            {
+                if (_char != value)
+                {
+                    _char = value;
+                    UpdateSize();
+                    InvalidateVisual();
+                }
+            }
+        }
+
+        static SpriteChar()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(
+                typeof(SpriteChar),
+                new FrameworkPropertyMetadata(typeof(SpriteChar)));
+        }
+
+        public SpriteChar()
+        {
+            Loaded += SpriteText_Loaded;
+            Unloaded += SpriteText_Unloaded;
+            UpdateSize();
+        }
+
+        private void UpdateSize()
+        {
+            var typeface = new Typeface(FontFamily, FontStyle, FontWeight, FontStretch);
+            var formattedText = new FormattedText(
+                Char.ToString(),
+                System.Globalization.CultureInfo.CurrentUICulture,
+                FlowDirection.LeftToRight,
+                typeface,
+                FontSize,
+                Foreground,
+                VisualTreeHelper.GetDpi(this).PixelsPerDip);
+
+            Width = formattedText.WidthIncludingTrailingWhitespace;
+            Height = formattedText.Height;
+        }
+
+        private void SpriteText_Loaded(object sender, RoutedEventArgs e)
+        {
+            _animationStartTime = DateTime.Now;
+            _interval = DesignDetail.Text.SpriteText.CreateInterval;
+            AnimateAppearance(true);
+        }
+
+        private void SpriteText_Unloaded(object sender, RoutedEventArgs e)
+        {
+            _animationStartTime = DateTime.Now;
+            _interval = DesignDetail.Text.SpriteText.DestroyInterval;
+            AnimateAppearance(false);
+        }
+
+        private void AnimateAppearance(bool appearing)
+        {
+            var fromOpacity = appearing ? 0.0 : 1.0;
+            var toOpacity = appearing ? 1.0 : 0.0;
+            var duration = new Duration(TimeSpan.FromSeconds(_interval));
+
+            // 确保有 RenderTransform
+            if (this.RenderTransform is not TranslateTransform translate)
+            {
+                translate = new TranslateTransform();
+                this.RenderTransform = translate;
+            }
+
+            // Opacity 动画
+            var opacityAnimation = new DoubleAnimation(fromOpacity, toOpacity, duration)
+            {
+                FillBehavior = FillBehavior.Stop
+            };
+
+            // Y 方向动画（仅删除时）
+            DoubleAnimation? yAnimation = null;
+            if (!appearing)
+            {
+                yAnimation = new DoubleAnimation(0, 30, duration) // 30 可调整为你想要的掉落距离
+                {
+                    FillBehavior = FillBehavior.Stop
+                };
+                yAnimation.Completed += (s, e) =>
+                {
+                    this.Visibility = Visibility.Collapsed;
+                    translate.Y = 0; // 复位
+                };
+            }
+            else
+            {
+                this.Visibility = Visibility.Visible;
+                translate.Y = 0;
+            }
+
+            opacityAnimation.Completed += (s, e) =>
+            {
+                if (!appearing)
+                    this.Opacity = 0;
+                else
+                    this.Opacity = 1;
+            };
+
+            this.BeginAnimation(OpacityProperty, opacityAnimation);
+            if (yAnimation != null)
+                translate.BeginAnimation(TranslateTransform.YProperty, yAnimation);
+        }
+
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+
+            var typeface = new Typeface(FontFamily, FontStyle, FontWeight, FontStretch);
+            var formattedText = new FormattedText(
+                Char.ToString(),
+                System.Globalization.CultureInfo.CurrentUICulture,
+                FlowDirection.LeftToRight,
+                typeface,
+                FontSize,
+                Foreground,
+                VisualTreeHelper.GetDpi(this).PixelsPerDip);
+            
+            //Render shadow if enabled
+            if (HasShadow)
+            {
+                var shadowBrush = new SolidColorBrush(ShadowColor);
+                drawingContext.DrawText(
+                    new FormattedText(
+                        Char.ToString(),
+                        System.Globalization.CultureInfo.CurrentUICulture,
+                        FlowDirection.LeftToRight,
+                        typeface,
+                        FontSize,
+                        shadowBrush,
+                        VisualTreeHelper.GetDpi(this).PixelsPerDip),
+                    new System.Windows.Point(ShadowOffset.X, ShadowOffset.Y));
+            }
+            //Render the character itself
+            drawingContext.DrawText(formattedText, new System.Windows.Point(0, 0));
+        }
+
+        public void PlayDeleteAnimation(Action? onCompleted = null)
+        {
+            var interval = DesignDetail.Text.SpriteText.DestroyInterval;
+            var fromOpacity = 1.0;
+            var toOpacity = 0.0;
+            var duration = new Duration(TimeSpan.FromSeconds(interval));
+
+            if (this.RenderTransform is not TranslateTransform translate)
+            {
+                translate = new TranslateTransform();
+                this.RenderTransform = translate;
+            }
+
+            var opacityAnimation = new DoubleAnimation(fromOpacity, toOpacity, duration)
+            {
+                FillBehavior = FillBehavior.Stop
+            };
+            var yAnimation = new DoubleAnimation(0, 10, duration)
+            {
+                FillBehavior = FillBehavior.Stop
+            };
+
+            int completedCount = 0;
+            void OnAnyCompleted(object? s, EventArgs e)
+            {
+                completedCount++;
+                if (completedCount == 2)
+                {
+                    this.Visibility = Visibility.Collapsed;
+                    translate.Y = 0;
+                    this.Opacity = 0;
+                    onCompleted?.Invoke();
+                }
+            }
+
+            opacityAnimation.Completed += OnAnyCompleted;
+            yAnimation.Completed += OnAnyCompleted;
+
+            this.BeginAnimation(OpacityProperty, opacityAnimation);
+            translate.BeginAnimation(TranslateTransform.YProperty, yAnimation);
+        }
+    }
+}
