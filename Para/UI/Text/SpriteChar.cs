03f4d0ad15ecc690617c4d7bb363f1169c84c1b2
CattyCathy <157694740+CattyCathy@users.noreply.github.com>
2025-05-28 20:59:09 +0800

    Formatted Code of SpriteChar

--- Para/UI/Text/SpriteChar.cs
+++ Para/UI/Text/SpriteChar.cs
@@ -16,10 +16,12 @@ namespace Para.UI.Text
     /// </summary>
     public class SpriteChar : BeatSyncedControl
     {
-        private char _char = ' ';
+        // Local values for public properties
+        protected char _char = ' ';
 
-        private double _interval;
-        private DateTime _animationStartTime = DateTime.Now;
+        // Runtime values
+        protected double _interval;
+        protected DateTime _animationStartTime = DateTime.Now;
 
         // Public settable properties
         public bool HasShadow;
@@ -60,7 +62,67 @@ namespace Para.UI.Text
             AnimateAppearance(true);
         }
 
-        private void UpdateSize()
+        protected void SpriteText_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+        protected void SpriteText_Unloaded(object sender, RoutedEventArgs e)
+        {
+            _animationStartTime = DateTime.Now;
+            _interval = DesignDetail.Text.SpriteChar.DestroyInterval;
+            AnimateAppearance(false);
+        }
+
+
+        // Events
+
+        // Events: Rendering
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+
+            var typeface = new Typeface(FontFamily, FontStyle, FontWeight, FontStretch);
+            var formattedText = new FormattedText(
+                Char.ToString(),
+                System.Globalization.CultureInfo.CurrentUICulture,
+                FlowDirection.LeftToRight,
+                typeface,
+                FontSize,
+                Foreground,
+                VisualTreeHelper.GetDpi(this).PixelsPerDip);
+
+            //Render shadow if enabled
+            if (HasShadow)
+            {
+                var shadowBrush = new SolidColorBrush(ShadowColor);
+                drawingContext.DrawText(
+                    new FormattedText(
+                        Char.ToString(),
+                        System.Globalization.CultureInfo.CurrentUICulture,
+                        FlowDirection.LeftToRight,
+                        typeface,
+                        FontSize,
+                        shadowBrush,
+                        VisualTreeHelper.GetDpi(this).PixelsPerDip),
+                    new System.Windows.Point(ShadowOffset.X, ShadowOffset.Y));
+            }
+            //Render the character itself
+            drawingContext.DrawText(formattedText, new System.Windows.Point(0, 0));
+        }
+
+
+        // Method
+
+        // Method: Visual
+        /// <summary>
+        /// Though it has no code, don't remove this method or TextBox will not be able to animate the deletion of this character.
+        /// </summary>
+        /// <param name="onCompleted"></param>
+        public void PlayDeleteAnimation(Action? onCompleted = null)
+        {
+
+        }
+        protected void UpdateSize()
         {
             var typeface = new Typeface(FontFamily, FontStyle, FontWeight, FontStretch);
             var formattedText = new FormattedText(
@@ -75,24 +137,11 @@ namespace Para.UI.Text
             Width = formattedText.WidthIncludingTrailingWhitespace;
             Height = formattedText.Height;
         }
-
-        private void SpriteText_Loaded(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void SpriteText_Unloaded(object sender, RoutedEventArgs e)
-        {
-            _animationStartTime = DateTime.Now;
-            _interval = DesignDetail.Text.SpriteChar.DestroyInterval;
-            AnimateAppearance(false);
-        }
-
         /// <summary>
         /// Animation for appearance and disappearance of the character.
         /// </summary>
         /// <param name="appearing">If the character is apperaing</param>
-        private void AnimateAppearance(bool appearing)
+        protected void AnimateAppearance(bool appearing)
         {
             var fromOpacity = appearing ? 0.0 : 1.0;
             var toOpacity = appearing ? 1.0 : 0.0;
@@ -140,49 +189,6 @@ namespace Para.UI.Text
             this.BeginAnimation(OpacityProperty, opacityAnimation);
             if (yAnimation != null)
                 translate.BeginAnimation(TranslateTransform.YProperty, yAnimation);
-        }
-
-
-        protected override void OnRender(DrawingContext drawingContext)
-        {
-            base.OnRender(drawingContext);
-
-            var typeface = new Typeface(FontFamily, FontStyle, FontWeight, FontStretch);
-            var formattedText = new FormattedText(
-                Char.ToString(),
-                System.Globalization.CultureInfo.CurrentUICulture,
-                FlowDirection.LeftToRight,
-                typeface,
-                FontSize,
-                Foreground,
-                VisualTreeHelper.GetDpi(this).PixelsPerDip);
-            
-            //Render shadow if enabled
-            if (HasShadow)
-            {
-                var shadowBrush = new SolidColorBrush(ShadowColor);
-                drawingContext.DrawText(
-                    new FormattedText(
-                        Char.ToString(),
-                        System.Globalization.CultureInfo.CurrentUICulture,
-                        FlowDirection.LeftToRight,
-                        typeface,
-                        FontSize,
-                        shadowBrush,
-                        VisualTreeHelper.GetDpi(this).PixelsPerDip),
-                    new System.Windows.Point(ShadowOffset.X, ShadowOffset.Y));
-            }
-            //Render the character itself
-            drawingContext.DrawText(formattedText, new System.Windows.Point(0, 0));
-        }
-
-        /// <summary>
-        /// Though it has no code, don't remove this method or TextBox will not be able to animate the deletion of this character.
-        /// </summary>
-        /// <param name="onCompleted"></param>
-        public void PlayDeleteAnimation(Action? onCompleted = null)
-        {
-
         }
     }
 }
